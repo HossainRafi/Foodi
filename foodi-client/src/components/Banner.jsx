d0616a23e6e29b8f77a3ebd1f35c96d6e1cd8a72
HossainRafi <hossainrafi71@gmail.com>
2024-06-26 23:07:33 +0600

    banner floating

--- foodi-client/src/components/Banner.jsx
+++ foodi-client/src/components/Banner.jsx
@@ -1,10 +1,11 @@
 import banner from "/images/home/banner.png";
-import floatingImg from "/images/home/b-food1.png";
+import floatingImg1 from "/images/home/b-food1.png";
+import floatingImg2 from "/images/home/b-food2.png";
 
 export const Banner = () => {
   return (
     <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
-      <div className="py-24 flex flex-col md:flex-row justify-between items-center gap-8">
+      <div className="py-20 flex flex-col md:flex-row justify-between items-center gap-8">
         {/* text section (left) */}
         <div className="md:w-1/2 space-y-7 px-4">
           <h2 className="text-4xl md:text-5xl font-bold md:leading-snug leading-snug capitalize">
@@ -24,11 +25,77 @@ export const Banner = () => {
         <div className="md:w-1/2">
           <img src={banner} />
 
-          <div>
-            <div>
-              <img src={floatingImg} className="rounded-2xl" />
-              <div>
-                <h5 className="capitalize ">spicy noodles</h5>
+          <div className="flex items-center justify-around flex-col md:flex-row -mt-14 gap-4">
+            <div className="flex bg-white py-1 px-1 rounded-2xl items-center gap-3 shadow-md w-60">
+              <img src={floatingImg1} className="rounded-2xl h-[80px]" />
+              <div className="space-y-0">
+                <h5 className="capitalize font-medium mb-1">vegitable salad</h5>
+                {/* ratting div */}
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                    defaultChecked
+                  />
+                </div>
+                <p className="text-red">$15.00</p>
+              </div>
+            </div>
+            <div className="flex bg-white py-1 px-1 rounded-2xl items-center gap-3 shadow-md w-60">
+              <img src={floatingImg2} className="rounded-2xl h-[80px]" />
+              <div className="space-y-0">
+                <h5 className="capitalize font-medium mb-1">egg fruit salad</h5>
+                {/* ratting div */}
+                <div className="rating rating-sm">
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                    defaultChecked
+                  />
+                  <input
+                    type="radio"
+                    name="rating-2"
+                    className="mask mask-star-2 bg-yellow-400"
+                  />
+                </div>
+                <p className="text-red">$25.00</p>
               </div>
             </div>
           </div>
