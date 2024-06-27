80948b04b2ad6ad413957cd6e8744a84713ef7bd
HossainRafi <hossainrafi71@gmail.com>
2024-06-27 12:42:44 +0600

    menudata added

--- foodi-client/src/pages/home/SpecialDishes.jsx
+++ foodi-client/src/pages/home/SpecialDishes.jsx
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export const SpecialDishes = () => {
+
   // carousel settings
   const settings = {
     dots: true,
