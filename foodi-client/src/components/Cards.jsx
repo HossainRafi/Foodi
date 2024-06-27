fafc9aa3e4d9bdbc428b51f85581a9eec52416f1
HossainRafi <hossainrafi71@gmail.com>
2024-06-27 13:41:13 +0600

    toggle heart

--- foodi-client/src/components/Cards.jsx
+++ foodi-client/src/components/Cards.jsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export const Cards = ({ item }) => {
+  const [isHeartFillted, setIsHeartFillted] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsHeartFillted(!isHeartFillted);
+  };
+
   return (
-    <div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl relative">
+      <div
+        className={`rating gap-1 absolute right-0 top-0 p-4 heartStar bg-green ${
+          isHeartFillted ? "text-rose-500" : "text-white"
+        }`}
+        onClick={handleHeartClick}
+      >
+        <FaHeart className="h-5 w-5 cursor-pointer" />
+      </div>
+      <Link to={`/menu/${item._id}`}>
         <figure>
-          <img src={item?.image} />
+          <img
+            src={item?.image}
+            className="hover:scale-105 transition-all duration-300 md:h-72"
+          />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">{item.name}</h2>
-          <p>description of the item</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+      </Link>
+      <div className="card-body">
+        <h2 className="card-title">{item.name}</h2>
+        <p>description of the item</p>
+        <div className="card-actions justify-between items-center mt-2">
+          <h5 className="font-semibold">
+            <span className="text-md text-red">$ </span>
+            {item.price}
+          </h5>
+          <button className="btn bg-green text-white">Buy Now</button>
         </div>
       </div>
     </div>
