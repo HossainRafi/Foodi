cd85ceeab3a3a3a986d99f88f20dcf28b37f0e2a
HossainRafi <hossainrafi71@gmail.com>
2024-06-28 23:13:58 +0600

    sorting done

--- foodi-client/src/pages/shop/Menu.jsx
+++ foodi-client/src/pages/shop/Menu.jsx
@@ -96,7 +96,7 @@ export const Menu = () => {
         {/* menu sorting filter btns */}
         <div>
           {/* all category btns */}
-          <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap mb-8">
+          <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
             <button
               onClick={showAll}
               className={selectedCategory === "all" ? "active" : ""}
@@ -135,12 +135,17 @@ export const Menu = () => {
             </button>
           </div>
 
-          {/* sorting filter */}
-          <div>
-            <FaFilter className="text-white h-4 w-4" />
-
-            {/* sorting options */}
-            <select name="sort" id="sort">
+          {/* filter options */}
+          <div className="flex justify-end mb-4 rounded-sm">
+            <div className="bg-black p-2 ">
+              <FaFilter className="text-white h-4 w-4" />
+            </div>
+            <select
+              id="sort"
+              onChange={(e) => handleSortChange(e.target.value)}
+              value={sortOption}
+              className="bg-black text-white px-2 py-1 rounded-sm outline-none cursor-pointer"
+            >
               <option value="default"> Default</option>
               <option value="A-Z">A-Z</option>
               <option value="Z-A">Z-A</option>
