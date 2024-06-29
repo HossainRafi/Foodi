5e0f61e33d19b185fb4a21486c657a86c18e6d10
HossainRafi <hossainrafi71@gmail.com>
2024-06-29 16:13:19 +0600

    setup for authentication

--- foodi-client/src/context/AuthProvider.jsx
+++ foodi-client/src/context/AuthProvider.jsx
@@ -1,12 +1,19 @@
+import { getAuth } from "firebase/auth";
 import { createContext, useState } from "react";
+import app from "./../firebase/firebase.config";
 
+// export authContext
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  const [loading,setLoading]=useState(true)
+
+  
 
   const authInfo = {
-    user
+    user,
   };
 
   return (
