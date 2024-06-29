4a4fb354db3e5087d9c696d5373209dbf6ef7788
HossainRafi <hossainrafi71@gmail.com>
2024-06-30 00:39:04 +0600

    api_key problem solved

--- foodi-client/src/firebase/firebase.config.js
+++ foodi-client/src/firebase/firebase.config.js
@@ -1,14 +1,25 @@
 import { initializeApp } from "firebase/app";
 
-// console.log(import.meta.env.VITE_SOME_KEY)
 
+// Firebase Config File
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APIKEY,
+//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+//   projectId: import.meta.env.VITE_PROJECTID,
+//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+//   appId: import.meta.env.VITE_APPID,
+// };
+
+// ================================
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID,
+  apiKey: "AIzaSyBwoLFDWrWeeRu1B-cdnrsA8bGYWgO4raM",
+  authDomain: "foodi-2b6d3.firebaseapp.com",
+  projectId: "foodi-2b6d3",
+  storageBucket: "foodi-2b6d3.appspot.com",
+  messagingSenderId: "810909652107",
+  appId: "1:810909652107:web:a74bf7930f59e6a9bff08e",
 };
 
 // Initialize Firebase
