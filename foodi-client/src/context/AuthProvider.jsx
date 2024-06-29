2f0a400c1f3c53955668a3cb871a6f7a4e34ac10
HossainRafi <hossainrafi71@gmail.com>
2024-06-29 16:22:01 +0600

    create providers and pass it

--- foodi-client/src/context/AuthProvider.jsx
+++ foodi-client/src/context/AuthProvider.jsx
@@ -1,19 +1,35 @@
-import { getAuth } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
 import { createContext, useState } from "react";
 import app from "./../firebase/firebase.config";
 
 // export authContext
 export const AuthContext = createContext();
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading,setLoading]=useState(true)
+  const [loading, setLoading] = useState(true);
 
-  
+  // create an account
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // signup with google
+  const signupWithGmail = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
 
   const authInfo = {
     user,
+    createUser,
+    signupWithGmail,
   };
 
   return (
