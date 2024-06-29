a8558e29f81263503e8e7c98e7def056a6939587
HossainRafi <hossainrafi71@gmail.com>
2024-06-29 08:53:48 +0600

    form cleanup

--- foodi-client/src/components/Modal.jsx
+++ foodi-client/src/components/Modal.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
 
 export const Modal = () => {
   return (
     <div>
       <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
         <div className="modal-box">
-          <div className="modal-action mt-0">
+          <div className="modal-action flex flex-col justify-center mt-0">
+            {/* login form */}
             <form className="card-body" method="dialog">
               <h3 className="font-bold text-lg capitalize">please login</h3>
               <div className="form-control">
@@ -40,19 +42,26 @@ export const Modal = () => {
 
               {/* login btn */}
               <div className="form-control mt-6">
-                <input type="submit" value="Login" className="btn bg-green" />
+                <input type="submit" value="Login" className="btn bg-green text-xl font-bold" />
               </div>
 
               <p className="text-center my-2">
                 Don't have an account?{" "}
                 <Link
                   to="/signup"
-                  className="text-green ml-1 bg-gray-100 p-1 rounded"
+                  className="text-green ml-1 bg-gray-100 p-1 rounded font-medium"
                 >
-                  Signup Now
+                  Signup
                 </Link>{" "}
               </p>
             </form>
+
+            <div className="text-center space-x-3 mb-5 flex items-center justify-center">
+              <p className="text-lg font-medium">Login with Google</p>
+              <button className="btn btn-circle hover:bg-green hover:text-white">
+                <FaGoogle />
+              </button>
+            </div>
           </div>
         </div>
       </dialog>
