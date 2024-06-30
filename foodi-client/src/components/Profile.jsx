01cc5786417c0a547ab463e901c9d0b86e899fc7
HossainRafi <hossainrafi71@gmail.com>
2024-06-30 09:35:33 +0600

    drawer

---
+++ foodi-client/src/components/Profile.jsx
@@ -0,0 +1,40 @@
+export const Profile = (user) => {
+  return (
+    <div>
+      <div className="drawer drawer-end z-50">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-4"
+            className="drawer-button btn btn-primary"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-4"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li>
+              <a href="/update-profile">Profile</a>
+            </li>
+            <li>
+              <a>Order</a>
+            </li>
+            <li>
+              <a>Setting</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
