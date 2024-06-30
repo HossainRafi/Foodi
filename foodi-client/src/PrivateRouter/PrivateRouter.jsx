09c1adac88690866b62aafb1374bcc2fba6bb20c
HossainRafi <hossainrafi71@gmail.com>
2024-06-30 20:45:14 +0600

    auth

--- foodi-client/src/PrivateRouter/PrivateRouter.jsx
+++ foodi-client/src/PrivateRouter/PrivateRouter.jsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { authContext } from "./../context/AuthProvider";
+import { AuthContext } from "./../context/AuthProvider";
 import { LoadingSpinner } from "./../components/LoadingSpinner";
 
 export const PrivateRouter = ({ children }) => {
-  const { user, loading } = useContext(authContext);
+  const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
   if (loading) {
