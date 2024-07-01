72670b5151670fbb8e85872ded0a500db2eae01f
HossainRafi <hossainrafi71@gmail.com>
2024-07-01 16:05:59 +0600

    cart page heading

--- foodi-client/src/pages/shop/CartPage.jsx
+++ foodi-client/src/pages/shop/CartPage.jsx
@@ -1,7 +1,15 @@
-
-
 export const CartPage = () => {
   return (
-    <div>CartPage</div>
-  )
-}
+    <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
+      <div className="py-24 flex flex-col justify-center items-center gap-8">
+        {/* text section (left) */}
+        <div className="space-y-7 px-4">
+          <h2 className="text-4xl md:text-5xl font-bold md:leading-snug leading-snug capitalize">
+            dive into delights of delectable{" "}
+            <span className="text-green">food</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
