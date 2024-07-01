cb73eab5067130d4f1bc7586a5b800dbe745c5db
HossainRafi <hossainrafi71@gmail.com>
2024-07-01 16:36:23 +0600

    customize table

--- foodi-client/src/pages/shop/CartPage.jsx
+++ foodi-client/src/pages/shop/CartPage.jsx
@@ -1,13 +1,64 @@
 export const CartPage = () => {
   return (
     <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
-      <div className="py-24 flex flex-col justify-center items-center gap-8">
-        {/* text section (left) */}
+      {/* banner */}
+      <div className="pt-32 pb-16 flex flex-col justify-center items-center gap-8">
         <div className="space-y-7 px-4">
           <h2 className="text-4xl md:text-5xl font-bold md:leading-snug leading-snug capitalize">
-            dive into delights of delectable{" "}
-            <span className="text-green">food</span>
+            items added to the{" "}
+            <span className="text-green block text-center">food cart</span>
           </h2>
+        </div>
+      </div>
+
+      {/* table */}
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-green text-white">
+              <tr className="text-xl">
+                <th>#</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <td>1</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-circle h-12 w-12">
+                        <img
+                          src="#"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Hart Hagerty</div>
+                      <div className="text-sm opacity-50">United States</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
