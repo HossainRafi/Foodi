ea8ef90d88a1fde6c98aa280db678906a649b31d
HossainRafi <hossainrafi71@gmail.com>
2024-07-01 09:59:25 +0600

    mongodb connected

--- foodi-server/index.js
+++ foodi-server/index.js
@@ -2,10 +2,40 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+console.log(process.env.DB_USER);
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+// mongodb configuration using mongoose
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodi.q8mmsmf.mongodb.net/?appName=Foodi`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // testing server is running or not
 app.get("/", (req, res) => {
@@ -14,6 +44,6 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(
-    `Web app listening on port ${port}......................................`
+    `..............................................................................APP IS LISTENING ON PORT ${port}`
   );
 });
