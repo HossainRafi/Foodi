253cc0c1dc07e1a986ac0a656b461905298ffa8c
HossainRafi <hossainrafi71@gmail.com>
2024-07-04 00:15:13 +0600

    connect database

--- foodi-server/index.js
+++ foodi-server/index.js
@@ -8,6 +8,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// hossainrafi71
+// b9d5fkJ8hhBhrs0k
+
+// mongodb configuration using mongoose
+mongoose
+  .connect(
+    `mongodb+srv://hossainrafi71:b9d5fkJ8hhBhrs0k@foodi.1etufpk.mongodb.net/?retryWrites=true&w=majority&appName=foodi`
+  )
+  .then(
+    console.log(
+      `............................................................................DATABASE CONNECTED SUCCESSFULLY`
+    )
+  )
+  .catch((error) => console.log("Error when connected to mongoDB", error));
+
 // testing surver is running or not
 app.get("/", (req, res) => {
   res.send("Hello World!");
