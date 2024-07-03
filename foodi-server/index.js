dc7fe6350c4bec888c331fc1d47269ed718daf13
HossainRafi <hossainrafi71@gmail.com>
2024-07-04 00:25:37 +0600

    secure secret keys

--- foodi-server/index.js
+++ foodi-server/index.js
@@ -2,26 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// hossainrafi71
-// b9d5fkJ8hhBhrs0k
-
 // mongodb configuration using mongoose
 mongoose
   .connect(
-    `mongodb+srv://hossainrafi71:b9d5fkJ8hhBhrs0k@foodi.1etufpk.mongodb.net/?retryWrites=true&w=majority&appName=foodi`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodi.1etufpk.mongodb.net/?retryWrites=true&w=majority&appName=foodi`
   )
   .then(
     console.log(
       `............................................................................DATABASE CONNECTED SUCCESSFULLY`
     )
   )
-  .catch((error) => console.log("Error when connected to mongoDB", error));
+  .catch((error) =>
+    console.log("Error occured when connected to mongoDB", error)
+  );
 
 // testing surver is running or not
 app.get("/", (req, res) => {
