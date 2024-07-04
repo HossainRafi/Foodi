a1c687e14aa77ac85682ab2270ab5c8d5a5bcefd
HossainRafi <hossainrafi71@gmail.com>
2024-07-04 07:26:37 +0600

    create add to cart controller

--- foodi-server/api/controllers/cartControllers.js
+++ foodi-server/api/controllers/cartControllers.js
@@ -13,7 +13,37 @@ const getCartByEmail = async (req, res) => {
   }
 };
 
+// post a cart when add-to-cart btn clicked
+const addToCart = async (req, res) => {
+  const { menuItemId, name, recipe, image, price, quantity, email } = req.body;
+  // console.log(email)
+  try {
+    // exiting menu item
+    const existingCartItem = await Carts.findOne({ email, menuItemId });
+    // console.log(existingCartItem)
+    if (existingCartItem) {
+      return res
+        .status(400)
+        .json({ message: "Already exists in the cart !" });
+    }
 
-module.exports={
-  getCartByEmail
-}
+    const cartItem = await Carts.create({
+      menuItemId,
+      name,
+      recipe,
+      image,
+      price,
+      quantity,
+      email,
+    });
+
+    res.status(201).json(cartItem);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getCartByEmail,
+  addToCart,
+};
