8b4d3e7a3c18236df4925ec1f4b2a919f6c699c6
HossainRafi <hossainrafi71@gmail.com>
2024-07-05 09:43:39 +0600

    setup drawer for admin panel

--- foodi-client/src/layout/DashboardLayout.jsx
+++ foodi-client/src/layout/DashboardLayout.jsx
@@ -1,7 +1,38 @@
-
+import { Link, Outlet } from "react-router-dom";
 
 export const DashboardLayout = () => {
   return (
-    <div>DashboardLayout</div>
-  )
-}
+    <div>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+
+          <Outlet />
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/users">All Users</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
